30c1a35455a10bd7bfcc7086a86018a7d25001e5
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-20 15:03:47 +0600

    First React Lesson - 02

--- practice_01/src/index.js
+++ practice_01/src/index.js
@@ -3,16 +3,5 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const text = "Hello World!";
-
-const elem = (
-  <div>
-    <h2 className="text">Text: {text}</h2>
-    <input type="text" />
-    <label htmlFor=""></label>
-    <button tabIndex={0}>Click</button>
-  </div>
-);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(elem);
+root.render(<App />);
