76dfd1e2f19838ec365f3c2004d81b53b326c3b6
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-22 22:55:44 +0600

    02 React Lesson / New Project

--- practice_02_project/src/components/app/app.js
+++ practice_02_project/src/components/app/app.js
@@ -2,6 +2,7 @@ import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import EmployersList from "../employers-list/employers-list";
+import EmployersAddForm from "../employers-add-form/employers-add-form";
 
 import "./app.css";
 
@@ -16,6 +17,7 @@ function App() {
       </div>
 
       <EmployersList />
+      <EmployersAddForm />
     </div>
   );
 }
