9555d884a7719636e6b493a82323ac68a4aab2ea
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-22 22:48:05 +0600

    02 React Lesson / New Project

--- practice_02_project/src/components/app/app.js
+++ practice_02_project/src/components/app/app.js
@@ -1,5 +1,7 @@
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployersList from "../employers-list/employers-list";
 
 import "./app.css";
 
@@ -10,7 +12,10 @@ function App() {
 
       <div className="search-panel">
         <SearchPanel />
+        <AppFilter />
       </div>
+
+      <EmployersList />
     </div>
   );
 }
