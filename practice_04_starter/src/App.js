43abed05ddb2789a694ee46a4503d839890e0cf9
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-31 22:50:55 +0600

    Practice 04 - useState, useEffect, useCallback

--- practice_04_starter/src/App.js
+++ practice_04_starter/src/App.js
@@ -38,17 +38,13 @@ const Slider = (props) => {
   return (
     <Container>
       <div className="slider w-50 m-auto">
-        {/* <img
-          className="d-block w-100"
-          src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
-          alt="slide"
-        /> */}
-
-        {getSomeImages().map((url, i) => {
+        {/* {getSomeImages().map((url, i) => {
           return (
             <img key={i} className="d-block w-100" src={url} alt="slide" />
           );
-        })}
+        })} */}
+
+        <Slide getSomeImages={getSomeImages} />
 
         <div className="text-center mt-5">
           Active slide {slide} <br /> {autoplay ? "auto" : null}
@@ -72,6 +68,22 @@ const Slider = (props) => {
         </div>
       </div>
     </Container>
+  );
+};
+
+const Slide = ({ getSomeImages }) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    setImages(getSomeImages());
+  }, [getSomeImages]);
+
+  return (
+    <>
+      {images.map((url, i) => (
+        <img key={i} className="d-block w-100" src={url} alt="slide" />
+      ))}
+    </>
   );
 };
 
