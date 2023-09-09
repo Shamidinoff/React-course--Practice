08ac6170e4b2e1477c57f7ee409bee5eca860e59
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-09 21:22:28 +0600

    Create Redux project

--- practice_05_redux/src/index.js
+++ practice_05_redux/src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+
+const initialState = 0;
+
+const reducer = (state, action) => {
+  if (action.type === "INC") {
+    return state + 1;
+  }
+
+  return 0;
+};
+
+let state = reducer(initialState, { type: "INC" });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <></>
   </React.StrictMode>
 );
