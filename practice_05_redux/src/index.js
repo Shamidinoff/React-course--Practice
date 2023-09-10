0c9edc38119a3276ce347252a743d23c046d3658
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-10 17:18:14 +0600

    add file actionCreators and bindActionCreator and add to React

--- practice_05_redux/src/index.js
+++ practice_05_redux/src/index.js
@@ -2,31 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, bindActionCreators } from "redux";
 import reducer from "./reducer";
-import * as actions from "./actions";
+import { Provider } from "react-redux";
 
-import Counter from "./Counter";
+import App from "./Components/App";
 
 const store = createStore(reducer);
-const { dispatch, subscribe, getState } = store;
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-const update = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Counter
-        counter={getState().value}
-        inc={inc}
-        dec={dec}
-        rnd={() => {
-          const value = Math.floor(Math.random() * 10);
-          rnd(value);
-        }}
-      />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-};
-
-update();
-
-subscribe(update);
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
