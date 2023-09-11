9c0de2d39c566d09c7fe56e519f57d9174fe4b15
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-11 22:32:08 +0600

    Add Hero Admin Panel - project

--- practice_08_hero/src/components/heroesList/HeroesList.js
+++ practice_08_hero/src/components/heroesList/HeroesList.js
@@ -2,6 +2,7 @@ import { useHttp } from "../../hooks/http.hook";
 import { useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { createSelector } from "reselect";
 
 import {
   heroesFetching,
@@ -20,16 +21,29 @@ import "./heroesList.scss";
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-  const filteredHeroes = useSelector((state) => {
-    if (state.filters.activeFilter === "all") {
-      return state.heroes.heroes;
-    } else {
-      return state.heroes.heroes.filter(
-        (item) => item.element === state.activeFilter
-      );
+  const filteredHeroesSelector = createSelector(
+    (state) => state.filters.activeFilter,
+    (state) => state.heroes.heroes,
+    (filter, heroes) => {
+      if (filter === "all") {
+        return heroes;
+      } else {
+        return heroes.filter((item) => item.element === filter);
+      }
     }
-  });
+  );
 
+  // const filteredHeroes = useSelector((state) => {
+  //   if (state.filters.activeFilter === "all") {
+  //     return state.heroes.heroes;
+  //   } else {
+  //     return state.heroes.heroes.filter(
+  //       (item) => item.element === state.activeFilter
+  //     );
+  //   }
+  // });
+
+  const filteredHeroes = useSelector(filteredHeroesSelector);
   const heroesLoadingStatus = useSelector((state) => state.heroesLoadingStatus);
   const dispatch = useDispatch();
   const { request } = useHttp();
