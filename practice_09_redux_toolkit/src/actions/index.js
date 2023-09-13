71f69ad42ec0af3dff7f48f548d261d560db0bdf
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-13 22:52:29 +0600

    Add Hero Admin Panel - project // Redux Toolkit

--- practice_09_redux_toolkit/src/actions/index.js
+++ practice_09_redux_toolkit/src/actions/index.js
@@ -1,15 +1,12 @@
-import {heroesFetching, heroesFetched, heroesFetchingError} from '../components/heroesList/heroesSlice';
-import {filtersFetching, filtersFetched, filtersFetchingError} from '../components/heroesFilters/filtersSlice';
-export const fetchHeroes = (request) => (dispatch) => {
-    dispatch(heroesFetching());
-    request("http://localhost:3001/heroes")
-        .then(data => dispatch(heroesFetched(data)))
-        .catch(() => dispatch(heroesFetchingError()))
-}
+import {
+  filtersFetching,
+  filtersFetched,
+  filtersFetchingError,
+} from "../components/heroesFilters/filtersSlice";
 
 export const fetchFilters = (request) => (dispatch) => {
-    dispatch(filtersFetching());
-    request("http://localhost:3001/filters")
-        .then(data => dispatch(filtersFetched(data)))
-        .catch(() => dispatch(filtersFetchingError()))
-}
+  dispatch(filtersFetching());
+  request("http://localhost:3001/filters")
+    .then((data) => dispatch(filtersFetched(data)))
+    .catch(() => dispatch(filtersFetchingError()));
+};
